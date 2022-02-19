c6bbc07f2fb7fdbe946b190898626c5755f803b1
Gabriel Rodrigues <gabrielws31@gmail.com>
2022-02-19 15:15:29 -0300

    adding facebook tabs

--- facebook_clone/lib/screens/home_screen.dart
+++ facebook_clone/lib/screens/home_screen.dart
@@ -7,127 +7,88 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import '../config/palette.dart';
 
 class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
 
-  const HomeScreen({Key? key}) : super(key: key);
+  final int _iconSize = 28;
+
+  final List<IconData> tabsIcon = [
+    MdiIcons.home,
+    MdiIcons.televisionPlay,
+    MdiIcons.storeOutline,
+    MdiIcons.accountCircleOutline,
+    MdiIcons.bellOutline,
+    MdiIcons.menu,
+  ];
+
+  _getTabs() {
+    List<Tab> tabs  = [];
+    for (IconData icon in tabsIcon) {
+      tabs.add(Tab(icon: Icon(icon, size: 28)));
+    }
+    return tabs;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 6,
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              floating: true,
-              actions: [
-                CircleButton(
-                    icon: Icons.search,
-                    iconSize: 30.0,
-                    onPressed: (){
-
-                    }
-                ),
-                CircleButton(
-                    icon: MdiIcons.facebookMessenger,
-                    iconSize: 30.0,
-                    onPressed: (){
-
-                    }
+    return DefaultTabController(
+      length: 6,
+      child: Builder(builder: (BuildContext context) {
+        final TabController tabController = DefaultTabController.of(context)!;
+        tabController.addListener(() {
+          if (!tabController.indexIsChanging) {
+            // Your code goes here.
+            // To get index of current tab use tabController.index
+          }
+        });
+        return Scaffold(
+          body: NestedScrollView(
+            headerSliverBuilder: (context, value) {
+              return [
+                SliverAppBar(
+                  backgroundColor: Colors.white,
+                  floating: false,
+                  pinned: true,
+                  elevation: 0,
+                  actions: [
+                    CircleButton(
+                        icon: Icons.search, iconSize: 30.0, onPressed: () {}),
+                    CircleButton(
+                        icon: MdiIcons.facebookMessenger,
+                        iconSize: 30.0,
+                        onPressed: () {})
+                  ],
+                  systemOverlayStyle: const SystemUiOverlayStyle(
+                    statusBarBrightness: Brightness.light,
+                    statusBarColor: Colors.white,
+                  ),
+                  title: const Text("facebook",
+                      style: TextStyle(
+                        color: Palette.facebookBlue,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: -1.2,
+                      )),
+                  bottom: TabBar(
+                    labelColor: Palette.facebookBlue,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: _getTabs(),
+                  ),
                 )
+              ];
+            },
+            body: TabBarView(
+              children: [
+                Text("aaa"),
+                Text("Watch"),
+                Text("Marketplace"),
+                Text("Profile"),
+                Text("Notificaï¿½ï¿½es"),
+                Text("Menu")
               ],
-              systemOverlayStyle:const SystemUiOverlayStyle(
-                statusBarBrightness: Brightness.light,
-                statusBarColor: Colors.white,
-              ) ,
-              title: const Text(
-                  "facebook",
-                  style: TextStyle(
-                    color: Palette.facebookBlue,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -1.2,
-                  )
-              ),
-              bottom: TabBar(
-                  labelColor: Palette.facebookBlue,
-                  unselectedLabelColor: Colors.grey,
-                  tabs: [
-                    IconButton(
-                        onPressed: (){
-
-                      },
-                      icon: const Icon(MdiIcons.home)
-                    ),
-                    IconButton(
-                        onPressed: (){
-
-                      },
-                      icon: const Icon(
-                          MdiIcons.home,
-                      )
-                    ),
-                    IconButton(
-                        onPressed: (){
-
-                      },
-                      icon: const Icon(MdiIcons.home)
-                    ),
-                    IconButton(
-                        onPressed: (){
-
-                      },
-                      icon: const Icon(MdiIcons.accountCircle)
-                    ),
-                    IconButton(
-                        onPressed: (){
-
-                      },
-                      icon: const Icon(MdiIcons.bell)
-                    ),
-                    IconButton(
-                        onPressed: (){
-
-                      },
-                      icon: const Icon(MdiIcons.menu)
-                    )
-                  ],
-                ),
             ),
-            SliverToBoxAdapter(
-              child:Expanded(
-                child: TabBarView(
-                  children: [
-                    CreatePostContainer(
-                        currentUser: currentUser
-                    ),
-                    Container(
-                      height: 100,
-                      child: Text("Watch"),
-                    ),
-                    Container(
-                      height: 100,
-                      child: Text("Marketplace"),
-                    ),
-                    Container(
-                      height: 100,
-                      child: Text("Profile"),
-                    ),
-                    Container(
-                      height: 100,
-                      child: Text("Notificações"),
-                    ),
-                    Container(
-                      height: 100,
-                      child: Text("Menu"),
-                    )
-                  ],
-                ),
-              )
-            )
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
